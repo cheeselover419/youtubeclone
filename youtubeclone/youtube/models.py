f9e2c0cae7855f7f1d512fb5d0ab871428055326
aleksejkrukov <kryukov476@gmail.com>
2024-02-26 00:51:50 +0300

    added authentication

--- youtubeclone/youtube/models.py
+++ youtubeclone/youtube/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+class Channel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    subscribers = models.ManyToManyField(User, related_name='subscribed_channels', blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='images/default_pfp.jpg')
+
+    def __str__(self):
+        return self.name
--- youtubeclone/youtube/views.py
+++ youtubeclone/youtube/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+from .models import Channel
+
 
 # Create your views here.
 
@@ -23,7 +27,59 @@ def create_user(request):
     return render(request, 'create_user.html', {'form': form})
 
 def custom_login(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
 
 def custom_logout(request):
-    pass
+    logout(request)
+    return redirect('home')
+
+
+def channel(request, username, pk):
+    user = User.objects.get(username=username)
+    channel = Channel.objects.get(user=user, id=pk)
+
+    if request.method == 'POST':
+        action = request.POST['subscribe']
+
+        if action == 'unsubscribe':
+            channel.subscribers.remove(request.user)
+        else:
+            channel.subscribers.add(request.user)
+
+        channel.save()
+
+    return render(request, 'channel.html', {'channel': channel})
+
+
+def create_channel(request):
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            name = request.POST["channelName"]
+            pfp = request.FILES.get('channel_pfp')
+
+            if name and pfp:
+                channel = Channel(user=request.user, name=name, profile_picture=pfp)
+                channel.save()
+
+                return redirect('home')
+
+        else:
+            return render(request, 'create_channel.html')
+    else:
+        return redirect('login')
+
+    return render(request, 'create_channel.html')
